6c9063daef8627167dc272c71ece5ad654850639
Srivans7 <102616243+Srivans7@users.noreply.github.com>
2026-05-18 02:06:30 +0530

    feat: production settings for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -26,12 +26,12 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g3hj_aio9n#vk)5as7pvm_lvn!=mx&dzxkymb+o(wfo8bfhha-'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-g3hj_aio9n#vk)5as7pvm_lvn!=mx&dzxkymb+o(wfo8bfhha-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICSFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media configuration for user-uploaded documents.
 MEDIA_URL = '/media/'
